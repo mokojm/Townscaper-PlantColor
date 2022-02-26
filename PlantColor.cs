e8ac33863ec399e93dea2d4ac0055de6be5a366a
mokojm <73483001+mokojm@users.noreply.github.com>
2022-02-26 13:25:27 +0100

    First release

---
+++ PlantColor.cs
@@ -0,0 +1,66 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace PlantColor
+{
+    public class PlantColorMain : MelonMod
+    {
+
+		//Plants
+		public static Color defaultPlantColor = new Color (0.362f, 0.5569f, 0.4292f, 1f);
+		public static Color plantColor = new Color(0.362f, 0.5569f, 0.4292f, 1f);
+		public static Material plantMat;
+		public static bool isInitialized = false;
+
+
+
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			if (sceneName == "Placemaker")
+			{
+				MelonLogger.Msg("Main scene loaded");
+
+				// Initializing
+				//Initialize();
+				PlantsUI.Initialize(this);
+
+			}
+		}
+
+		public static void Initialize()
+        {
+
+			GameObject gameObject = GameObject.Find("Plants");
+			if (gameObject != null)
+			{
+				plantMat = gameObject.GetComponent<Placemaker.BigMeshGroup>().material;
+				plantColor = plantMat.color;
+				isInitialized = true;
+			}
+        }
+
+		public static void Reset()
+		{
+			plantColor.r = defaultPlantColor.r;
+			plantColor.g = defaultPlantColor.g;
+			plantColor.b = defaultPlantColor.b;
+			plantMat.color = plantColor;
+		}
+
+		public override void OnApplicationStart()
+		{
+			
+		}
+
+		public override void OnUpdate()
+		{
+			if (isInitialized == false)
+            {
+				Initialize();
+            }
+		}
+
+	}
+}
+
---
+++ PlantColorUI.cs
@@ -0,0 +1,51 @@
+﻿using MelonLoader;
+using UnityEngine;
+using ModUI;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+namespace PlantColor
+{
+	public static class PlantsUI
+	{
+		public static MelonMod myMod;
+		public static ModSettings myModSettings;
+		public static Slider referencedSlider;
+
+		public static Slider refPlantSliderR;
+		public static Slider refPlantSliderG;
+		public static Slider refPlantSliderB;
+
+		public static bool isInitialized;
+
+		public static void Initialize(MelonMod thisMod)
+		{
+
+			myModSettings = UIManager.Register(thisMod, new Color32(201, 245, 184, 255));
+
+			myModSettings.AddSlider("RED", "General", new Color32(119, 206, 224, 255), 0f, 1f, false, (float)PlantColorMain.plantColor.r, new Action<float>(delegate (float value) { PlantColorMain.plantColor.r = value; Update(); }));
+			myModSettings.AddSlider("GREEN", "General", new Color32(119, 206, 224, 255), 0f, 1f, false, (float)PlantColorMain.plantColor.g, new Action<float>(delegate (float value) { PlantColorMain.plantColor.g = value; Update(); }));
+			myModSettings.AddSlider("BLUE", "General", new Color32(119, 206, 224, 255), 0f, 1f, false, (float)PlantColorMain.plantColor.b, new Action<float>(delegate (float value) { PlantColorMain.plantColor.b = value; Update(); }));
+
+			myModSettings.AddButton("Reset", "General", new Color32(255, 179, 174, 255), new Action(delegate { PlantColorMain.Reset(); Update(); }));
+
+			refPlantSliderR = myModSettings.controlSliders["RED"].GetComponent<Slider>();
+			refPlantSliderG = myModSettings.controlSliders["GREEN"].GetComponent<Slider>();
+			refPlantSliderB = myModSettings.controlSliders["BLUE"].GetComponent<Slider>();
+
+            isInitialized = true;
+		}
+
+		public static void Update()
+		{
+			if (isInitialized)
+            {
+				PlantColorMain.plantMat.color = PlantColorMain.plantColor;
+				refPlantSliderR.value = (float)PlantColorMain.plantColor.r;
+				refPlantSliderG.value = (float)PlantColorMain.plantColor.g;
+				refPlantSliderB.value = (float)PlantColorMain.plantColor.b;
+			}
+		}
+	}
+}
